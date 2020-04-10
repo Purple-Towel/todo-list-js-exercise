1889df0452d43d38e51f76f5cac853a09282d5dc
Angad Grewal <angadgrewal31@gmail.com>
2020-04-10 20:38:34 +0000

    refactored code to use OOP logic

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescription = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      const title = this.title;
+      const complete = this.complete;
+      console.log(`${title} has${complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+//function completeTask(object) {
+//  object['complete'] = true;
+//}
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
+/*function logTaskState(object) {
+  const title = object['title'];
+  const complete = object['complete'];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
-
+*/
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Clean at 12pm");
+const task2 = newTask("Do Laundry", "Do at 1pm");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(tasks);
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
